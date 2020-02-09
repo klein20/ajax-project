edd5a4c4f9a177ecf41cd1347923579df07b154c
klein20 <olexandrkleyn@gmail.com>
2020-02-09 17:09:13 +0200

    added Redirect component

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,62 +2,62 @@ import React, { Component } from 'react';
 import axios from '../../../axios/axios';
 import { Route } from 'react-router-dom';
 
-import './Posts.css';
 import Post from '../../../components/Post/Post';
+import './Posts.css';
 import FullPost from '../FullPost/FullPost';
-
 
 class Posts extends Component {
     state = {
-        posts: [],
-        // selectedPostId: null,
-        // error: false
+        posts: []
     }
 
     componentDidMount () {
-        console.log(this.props);
+        console.log( this.props );
         axios.get( '/posts' )
             .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
+                const posts = response.data.slice( 0, 4 );
+                const updatedPosts = posts.map( post => {
                     return {
                         ...post,
                         author: 'Max'
                     }
-                });
-                this.setState({posts: updatedPosts});
+                } );
+                this.setState( { posts: updatedPosts } );
                 // console.log( response );
             } )
-            .catch(error => {
-                console.log(error);
-                //this.setState({error: true});
-            });
+            .catch( error => {
+                console.log( error );
+                // this.setState({error: true});
+            } );
     }
 
-    postSelectedHandler = (id) => {
+    postSelectedHandler = ( id ) => {
+        // this.props.history.push({pathname: '/posts/' + id});
         this.props.history.push( '/posts/' + id );
     }
 
-    render() {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
+    render () {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if ( !this.state.error ) {
+            posts = this.state.posts.map( post => {
                 return (
-                   // <Link to={'/' + post.id} >
-                        <Post key={post.id} 
-                        title={post.title} 
+                    // <Link to={'/posts/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
                         author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)} />
-                   // </Link>
-                   );
-            });
+                        clicked={() => this.postSelectedHandler( post.id )} />
+                    // </Link>
+                );
+            } );
         }
+
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
-                <Route path={this.props.match.url + '/: id'} exact component={FullPost} />
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
             </div>
         );
     }
