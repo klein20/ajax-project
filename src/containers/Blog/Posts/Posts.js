305857d8f354083fa0c969c230382db0b8b36b47
klein20 <olexandrkleyn@gmail.com>
2020-02-09 15:09:27 +0200

    added nested route and dynamic path

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from '../../../axios/axios';
-import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './Posts.css';
 import Post from '../../../components/Post/Post';
+import FullPost from '../FullPost/FullPost';
 
 
 class Posts extends Component {
@@ -34,7 +35,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.props.history.push( '/posts/' + id );
     }
 
     render() {
@@ -42,18 +43,22 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post 
+                   // <Link to={'/' + post.id} >
+                        <Post key={post.id} 
                         title={post.title} 
                         author={post.author}
                         clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>);
+                   // </Link>
+                   );
             });
         }
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/: id'} exact component={FullPost} />
+            </div>
         );
     }
 }
